773c6cad3b9cf78d8791b35119f9c70c752e560f
Joel Oliveira <joel@notifica.re>
2019-04-18 17:55:11 +0200

    - updated swift example with latest code

--- DemoSwift/AppDelegate.swift
+++ DemoSwift/AppDelegate.swift
@@ -10,20 +10,24 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, NotificarePushLibDelegate {
-
+    
     var window: UIWindow?
     var myString: String?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         NotificarePushLib.shared().initialize(withKey: nil, andSecret: nil)
         NotificarePushLib.shared().delegate = self
         NotificarePushLib.shared().launch()
+        if (launchOptions != nil) {
+            NotificarePushLib.shared().didFinishLaunching(options: launchOptions!)
+        }
+        
         
         if #available(iOS 10.0, *) {
             NotificarePushLib.shared().presentationOptions = .alert
         }
-
+        
         myString = "SOME STRING"
         return true
     }
@@ -67,29 +71,62 @@ class AppDelegate: UIResponder, UIApplicationDelegate, NotificarePushLibDelegate
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "didLoadInbox"), object: Any?.self)
     }
     
-
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        NotificarePushLib.shared().didRegisterForRemoteNotifications(withDeviceToken: deviceToken)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        //Handle Error
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        NotificarePushLib.shared().didReceiveRemoteNotification(userInfo, completionHandler: {(_ response: Any?, _ error: Error?) -> Void in
+            if error == nil {
+                completionHandler(.newData)
+            } else {
+                completionHandler(.noData)
+            }
+        })
+    }
+    
+    func application(_ application: UIApplication, handleActionWithIdentifier identifier: String?, forRemoteNotification userInfo: [AnyHashable : Any], withResponseInfo responseInfo: [AnyHashable : Any], completionHandler: @escaping () -> Void) {
+        NotificarePushLib.shared().handleAction(withIdentifier: identifier, forRemoteNotification: userInfo, withResponseInfo: responseInfo, completionHandler: {(_ response: Any?, _ error: Error?) -> Void in
+            completionHandler()
+        })
+    }
+    
+    func application(_ application: UIApplication, handleActionWithIdentifier identifier: String?, forRemoteNotification userInfo: [AnyHashable : Any], completionHandler: @escaping () -> Void) {
+        NotificarePushLib.shared().handleAction(withIdentifier: identifier, forRemoteNotification: userInfo, withResponseInfo: nil, completionHandler: {(_ response: Any?, _ error: Error?) -> Void in
+            completionHandler()
+        })
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    func printMyString() {
+        print("myString->\(String(describing: myString))")
+    }
+    
 }
 
